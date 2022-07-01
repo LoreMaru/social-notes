f77cc8456f16f2345475d82dea9b161271e03153
LoreMaru <gialma1120@hotmail.it>
2022-07-01 16:46:13 +0200

    Chore: first commit

---
+++ utils.js
@@ -0,0 +1,78 @@
+const c = (el) => document.createElement(el);
+
+const q = (el) => document.querySelector(el);
+
+const createCard = (parent, imgLink, title, price) => {
+  const wrapperEl = c("div");
+  const titleEl = c("h3");
+  const priceEl = c("p");
+  const imgEl = c("img");
+
+  wrapperEl.className = "wrapper";
+  titleEl.className = "title";
+  priceEl.className = "price";
+  imgEl.className = "img";
+
+  imgEl.setAttribute("alt", title);
+  imgEl.setAttribute("src", imgLink);
+
+  titleEl.textContent = title;
+  priceEl.textContent = price;
+
+  wrapperEl.append(imgEl, titleEl, priceEl);
+  parent.appendChild(wrapperEl);
+};
+
+const createFriendEl = (parent, name, photo) => {
+  const wrapper = c("div");
+  const img = c("img");
+  const par = c("p");
+
+  wrapper.className = "friendCard";
+  img.setAttribute("alt", name);
+  img.setAttribute("src", photo);
+  par.textContent = name;
+
+  wrapper.append(img, par);
+  parent.appendChild(wrapper);
+};
+
+const createMessageEl = (
+  parent,
+  id,
+  text,
+  sender,
+  date,
+  callback = () => {}
+) => {
+  const wrapper = c("div");
+  const textPar = c("p");
+  const senderPar = c("p");
+  const dataPar = c("p");
+
+  wrapper.className = "messageCard";
+  textPar.textContent = text;
+  senderPar.textContent = sender;
+  dataPar.textContent = date;
+
+  wrapper.addEventListener("click", callback);
+
+  wrapper.append(textPar, senderPar, dataPar);
+  parent.appendChild(wrapper);
+};
+
+const createInterestsEl = (parent, title, photo) => {
+  const wrapper = c("div");
+  const img = c("img");
+  const interestsTitle = c("p");
+
+  wrapper.className = "interestsCard";
+  img.setAttribute("alt", title);
+  img.setAttribute("src", photo);
+  interestsTitle.textContent = title;
+
+  wrapper.append(img, interestsTitle);
+  parent.appendChild(wrapper);
+};
+
+export { c, q, createCard, createFriendEl, createMessageEl, createInterestsEl };
